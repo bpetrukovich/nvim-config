0d1464014dca252658a79131d4b8132e410316cd
Bogdan Petrukovich <viorbrint@gmail.com>
2025-08-24 18:19:19 +0300

    codeium fix

--- lua/custom/plugins/codeium.lua
+++ lua/custom/plugins/codeium.lua
@@ -2,7 +2,7 @@ return {
   'Exafunction/windsurf.nvim',
   dependencies = {
     'nvim-lua/plenary.nvim',
-    'saghen/blink.cmp',
+    'hrsh7th/nvim-cmp',
   },
   config = function()
     require('codeium').setup {
