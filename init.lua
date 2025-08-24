0a38a96287fb0b0ba45114ead0f9d86c1e4a6ddf
Bogdan Petrukovich <viorbrint@gmail.com>
2025-08-24 17:29:30 +0300

    init.lua fix

--- init.lua
+++ init.lua
@@ -6,13 +6,10 @@ vim.g.maplocalleader = ' '
 vim.g.have_nerd_font = false
 
 -- Load configuration modules
-require('config.options')
-require('config.keymaps')
-require('config.autocmds')
-require('config.lazy')
-
--- Load custom configurations
-require('custom')
+require 'config.options'
+require 'config.keymaps'
+require 'config.autocmds'
+require 'config.lazy'
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
