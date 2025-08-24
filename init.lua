686922ece2cb6bb7ffb25a00efb7023c2f1c5a75
Bogdan Petrukovich <viorbrint@gmail.com>
2025-08-24 17:03:17 +0300

    todo-comments

--- init.lua
+++ init.lua
@@ -178,9 +178,6 @@ require('lazy').setup({
   -- which loads which-key before all the UI elements are loaded. Events can be
   -- normal autocommands events (`:help autocmd-events`).
 
-  -- Highlight todo, notes, etc in comments
-  { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
-
   { -- Collection of various small independent plugins/modules
     'echasnovski/mini.nvim',
     config = function()
