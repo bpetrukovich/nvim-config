7ba0261c9b3761af44843d3546f332ce6e6c388b
Bogdan Petrukovich <viorbrint@gmail.com>
2025-08-24 21:25:59 +0300

    linter

--- lua/custom/plugins/lint.lua
+++ lua/custom/plugins/lint.lua
@@ -7,6 +7,13 @@ return {
       local lint = require 'lint'
       lint.linters_by_ft = {
         markdown = { 'markdownlint' },
+        javascript = { 'eslint_d' },
+        typescript = { 'eslint_d' },
+        css = { 'stylelint' },
+        scss = { 'stylelint' },
+        sass = { 'stylelint' },
+        html = { 'htmlhint' },
+        -- htmlangular = { 'htmlhint' },
       }
 
       -- To allow other plugins to add linters to require('lint').linters_by_ft,
