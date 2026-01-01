176d94d074e6fd028203357d0d6f78b838010568
Bogdan Petrukovich <petrukovichbogdan@gmail.com>
2026-01-02 01:16:00 +0300

    eslint-lsp

--- lua/custom/plugins/lspconfig.lua
+++ lua/custom/plugins/lspconfig.lua
@@ -204,7 +204,12 @@ return { -- LSP Configuration & Plugins
     -- for you, so that they are available from within Neovim.
     local ensure_installed = vim.tbl_keys(servers or {})
     vim.list_extend(ensure_installed, {
+      'eslint-lsp',
+    })
+    vim.list_extend(ensure_installed, {
       'stylua', -- Used to format Lua code
+      'markdownlint',
+      -- 'eslint_d',
     })
     require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
