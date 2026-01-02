51998fe9ae3b3df36f4888ef98f1e62f92723a71
Bogdan Petrukovich <petrukovichbogdan@gmail.com>
2026-01-02 17:58:30 +0300

    telescope

--- lua/custom/plugins/telescope.lua
+++ lua/custom/plugins/telescope.lua
@@ -16,27 +16,12 @@ return { -- Fuzzy Finder (files, lsp, etc)
         return vim.fn.executable 'make' == 1
       end,
     },
-    -- TODO: maybe i dont need this since i have snacks
     { 'nvim-telescope/telescope-ui-select.nvim' },
 
     -- Useful for getting pretty icons, but requires a Nerd Font.
     { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
   },
   config = function()
-    -- The easiest way to use Telescope, is to start by doing something like:
-    --  :Telescope help_tags
-    -- After running this command, a window will open up and you're able to
-    -- type in the prompt window. You'll see a list of `help_tags` options and
-    -- a corresponding preview of the help.
-    --
-    -- Two important keymaps to use while in Telescope are:
-    --  - Insert mode: <c-/>
-    --  - Normal mode: ?
-    --
-    -- This opens a window that shows you all of the keymaps for the current
-    -- Telescope picker. This is really useful to discover what Telescope can
-    -- do as well as how to actually do it!
-
     -- [[ Configure Telescope ]]
     -- See `:help telescope` and `:help telescope.setup()`
     local open_with_trouble = require('trouble.sources.telescope').open
@@ -138,38 +123,5 @@ return { -- Fuzzy Finder (files, lsp, etc)
     vim.keymap.set('n', '<leader>sn', function()
       builtin.find_files { cwd = vim.fn.stdpath 'config' }
     end, { desc = '[S]earch [N]eovim files' })
-
-    -- Shortcut for searching your obsidian vault
-    -- vim.keymap.set('n', '<leader>so', function()
-    --   builtin.find_files { cwd = '/mnt/c/Users/b.petrukovich/Documents/Obsidian Vault/' }
-    -- end, { desc = '[S]earch [N]eovim files' })
-
-    local harpoon = require 'harpoon'
-    harpoon:setup {}
-
-    -- HARPOON
-    -- basic telescope configuration
-    -- local conf = require('telescope.config').values
-    -- local function toggle_telescope(harpoon_files)
-    --   local file_paths = {}
-    --   for _, item in ipairs(harpoon_files.items) do
-    --     table.insert(file_paths, item.value)
-    --   end
-    --
-    --   require('telescope.pickers')
-    --     .new({}, {
-    --       prompt_title = 'Harpoon',
-    --       finder = require('telescope.finders').new_table {
-    --         results = file_paths,
-    --       },
-    --       previewer = conf.file_previewer {},
-    --       sorter = conf.generic_sorter {},
-    --     })
-    --     :find()
-    -- end
-
-    -- vim.keymap.set('n', '<C-e>', function()
-    --   toggle_telescope(harpoon:list())
-    -- end, { desc = 'Open harpoon window' })
   end,
 }
