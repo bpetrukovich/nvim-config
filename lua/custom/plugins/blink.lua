d285871b98261ffc37d29dde0cd87522eaf748c4
Bogdan Petrukovich <petrukovichbogdan@gmail.com>
2026-01-02 17:40:31 +0300

    blink

--- lua/custom/plugins/blink.lua
+++ lua/custom/plugins/blink.lua
@@ -1,70 +1,66 @@
 return {
   'saghen/blink.cmp',
-  -- optional: provides snippets for the snippet source
-  dependencies = {
-    'rafamadriz/friendly-snippets',
-    'Kaiser-Yang/blink-cmp-avante',
-  },
-
-  -- use a release tag to download pre-built binaries
+  event = 'VimEnter',
   version = '1.*',
-  -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
-  -- build = 'cargo build --release',
-  -- If you use nix, you can build from source using latest nightly rust with:
-  -- build = 'nix run .#build-plugin',
-
-  ---@module 'blink.cmp'
-  ---@type blink.cmp.Config
+  dependencies = {
+    {
+      'L3MON4D3/LuaSnip',
+      version = '2.*',
+      build = (function()
+        if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
+          return
+        end
+        return 'make install_jsregexp'
+      end)(),
+      dependencies = {
+        {
+          'rafamadriz/friendly-snippets',
+          config = function()
+            require('luasnip.loaders.from_vscode').lazy_load()
+            require('luasnip.loaders.from_vscode').lazy_load {
+              paths = { vim.fn.stdpath 'config' .. '/snippets' },
+            }
+          end,
+        },
+      },
+      opts = {},
+    },
+    -- 'Kaiser-Yang/blink-cmp-avante',
+    -- 'Exafunction/windsurf.nvim',
+    'folke/lazydev.nvim',
+  },
+  --- @module 'blink.cmp'
+  --- @type blink.cmp.Config
   opts = {
-    -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
-    -- 'super-tab' for mappings similar to vscode (tab to accept)
-    -- 'enter' for enter to accept
-    -- 'none' for no mappings
-    --
-    -- All presets have the following mappings:
-    -- C-space: Open menu or open docs if already open
-    -- C-n/C-p or Up/Down: Select next/previous item
-    -- C-e: Hide menu
-    -- C-k: Toggle signature help (if signature.enabled = true)
-    --
-    -- See :h blink-cmp-config-keymap for defining your own keymap
-    keymap = { preset = 'default' },
+    keymap = {
+      preset = 'default',
+    },
 
     appearance = {
-      -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-      -- Adjusts spacing to ensure icons are aligned
       nerd_font_variant = 'mono',
     },
 
-    -- (Default) Only show the documentation popup when manually triggered
-    completion = { documentation = { auto_show = false } },
-
-    cmdline = {
-      enabled = false,
+    completion = {
+      documentation = { auto_show = false, auto_show_delay_ms = 500 },
     },
 
-    -- Default list of enabled providers defined so that you can extend it
-    -- elsewhere in your config, without redefining it, due to `opts_extend`
     sources = {
-
-      default = { 'avante', 'lsp', 'path', 'snippets', 'buffer', 'easy-dotnet' },
-
+      default = { 'lsp', 'path', 'snippets', 'buffer', 'easy-dotnet', 'lazydev' }, -- 'avante', 'codeium'
       providers = {
-        codeium = {
-          name = 'Codeium',
-          module = 'codeium.blink',
-          async = true,
-          enabled = function()
-            return vim.bo.filetype ~= 'oil'
-          end,
-        },
-        avante = {
-          module = 'blink-cmp-avante',
-          name = 'Avante',
-          opts = {
-            -- options for blink-cmp-avante
-          },
-        },
+        lazydev = { module = 'lazydev.integrations.blink', score_offset = 100 },
+        -- codeium = {
+        --   name = 'Codeium',
+        --   module = 'codeium.blink',
+        --   async = true,
+        --   enabled = function()
+        --     return vim.bo.filetype ~= 'oil'
+        --   end,
+        -- },
+        -- avante = {
+        --   module = 'blink-cmp-avante',
+        --   name = 'Avante',
+        --   opts = {},
+        -- },
         ['easy-dotnet'] = {
           name = 'easy-dotnet',
           enabled = true,
@@ -75,12 +71,10 @@ return {
       },
     },
 
-    -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
-    -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
-    -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
-    --
-    -- See the fuzzy documentation for more information
+    snippets = { preset = 'luasnip' },
+
     fuzzy = { implementation = 'prefer_rust_with_warning' },
+
+    signature = { enabled = true },
   },
-  opts_extend = { 'sources.default' },
 }
