2e6e46a244afe23465ce4787ea2339b074480e64
Bogdan Petrukovich <petrukovichbogdan@gmail.com>
2026-01-02 12:33:59 +0300

    add some mason plugins

--- lua/custom/plugins/lspconfig.lua
+++ lua/custom/plugins/lspconfig.lua
@@ -202,6 +202,7 @@ return { -- LSP Configuration & Plugins
     }
     -- You can add other tools here that you want Mason to install
     -- for you, so that they are available from within Neovim.
+    -- TODO: sort lsps, linters and formatters
     local ensure_installed = vim.tbl_keys(servers or {})
     vim.list_extend(ensure_installed, {
       'eslint-lsp',
@@ -209,6 +210,8 @@ return { -- LSP Configuration & Plugins
     vim.list_extend(ensure_installed, {
       'stylua', -- Used to format Lua code
       'markdownlint',
+      'prettierd',
+      'stylelint',
       -- 'eslint_d',
     })
     require('mason-tool-installer').setup { ensure_installed = ensure_installed }
