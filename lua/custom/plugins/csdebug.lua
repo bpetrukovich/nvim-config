831dbcf42059d2c830b7350a4aba3de38664c51e
Bogdan Petrukovich <petrukovichbogdan@gmail.com>
2026-01-02 12:32:12 +0300

    optimize csdebug

--- lua/custom/plugins/csdebug.lua
+++ lua/custom/plugins/csdebug.lua
@@ -1,4 +1,14 @@
 return {
   'nicholasmata/nvim-dap-cs',
+  ft = { 'cs', 'razor' },
   dependencies = { 'mfussenegger/nvim-dap' },
+  config = function()
+    require('dap-cs').setup {
+      netcoredbg = {
+        -- the path to the executable netcoredbg which will be used for debugging.
+        -- by default, this is the "netcoredbg" executable on your PATH.
+        path = vim.fn.expand '~/.local/share/nvim/mason/bin/netcoredbg',
+      },
+    }
+  end,
 }
