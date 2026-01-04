95a481ca3e1e5b68d8caa4e17d3a13bf42d8e775
Bogdan Petrukovich <petrukovichbogdan@gmail.com>
2026-01-05 02:10:23 +0300

    roslyn tweaks

--- lua/custom/plugins/roslyn.lua
+++ lua/custom/plugins/roslyn.lua
@@ -1,27 +1,70 @@
 return {
   'seblj/roslyn.nvim',
   ft = { 'cs', 'razor' },
-  -- opts = {
-  --   config = {
-  --     handlers = require 'rzls.roslyn_handlers',
-  --   },
-  -- },
+  opts = { filewatching = 'roslyn' },
   config = function()
-    local rzls_path = vim.fn.expand '$MASON/packages/rzls/libexec'
+    local handles = {}
+
+    vim.api.nvim_create_autocmd('User', {
+      pattern = 'RoslynRestoreProgress',
+      callback = function(ev)
+        local token = ev.data.params[1]
+        local handle = handles[token]
+        if handle then
+          handle:report {
+            title = ev.data.params[2].state,
+            message = ev.data.params[2].message,
+          }
+        else
+          handles[token] = require('fidget.progress').handle.create {
+            title = ev.data.params[2].state,
+            message = ev.data.params[2].message,
+            lsp_client = {
+              name = 'roslyn',
+            },
+          }
+        end
+      end,
+    })
+
+    vim.api.nvim_create_autocmd('User', {
+      pattern = 'RoslynRestoreResult',
+      callback = function(ev)
+        local handle = handles[ev.data.token]
+        handles[ev.data.token] = nil
+
+        if handle then
+          handle.message = ev.data.err and ev.data.err.message or 'Restore completed'
+          handle:finish()
+        end
+      end,
+    })
+
+    vim.api.nvim_create_autocmd({ 'InsertLeave' }, {
+      pattern = '*',
+      callback = function()
+        local clients = vim.lsp.get_clients { name = 'roslyn' }
+        for _, client in ipairs(clients) do
+          local buffers = vim.lsp.get_buffers_by_client_id(client.id)
+          for _, buf in ipairs(buffers) do
+            client:request('textDocument/diagnostic', {
+              textDocument = vim.lsp.util.make_text_document_params(buf),
+            })
+          end
+        end
+      end,
+    })
+
     local cmd = {
       'roslyn',
       '--stdio',
       '--logLevel=Information',
       '--extensionLogDirectory=' .. vim.fs.dirname(vim.lsp.get_log_path()),
-      '--razorSourceGenerator=' .. vim.fs.joinpath(rzls_path, 'Microsoft.CodeAnalysis.Razor.Compiler.dll'),
-      '--razorDesignTimePath=' .. vim.fs.joinpath(rzls_path, 'Targets', 'Microsoft.NET.Sdk.Razor.DesignTime.targets'),
       '--extension',
-      vim.fs.joinpath(rzls_path, 'RazorExtension', 'Microsoft.VisualStudioCode.RazorExtension.dll'),
     }
 
     vim.lsp.config('roslyn', {
       cmd = cmd,
-      handlers = require 'rzls.roslyn_handlers',
       settings = {
         ['csharp|inlay_hints'] = {
           csharp_enable_inlay_hints_for_implicit_object_creation = true,
@@ -47,13 +90,14 @@ return {
     })
     vim.lsp.enable 'roslyn'
   end,
-  init = function()
-    -- We add the Razor file types before the plugin loads.
-    vim.filetype.add {
-      extension = {
-        razor = 'razor',
-        cshtml = 'razor',
-      },
-    }
-  end,
+  -- init = function()
+  --   -- We add the Razor file types before the plugin loads.
+  --   vim.filetype.add {
+  --     extension = {
+  --       razor = 'razor',
+  --       cshtml = 'razor',
+  --     },
+  --   }
+  -- end,
+  --
 }
