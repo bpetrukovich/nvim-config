9f46ad0a11807883414fc27fa24387232c4dd0fb
Bogdan Petrukovich <petrukovichbogdan@gmail.com>
2026-01-05 03:29:11 +0300

    markdown-preview binding

--- lua/custom/plugins/markdown-preview.lua
+++ lua/custom/plugins/markdown-preview.lua
@@ -6,4 +6,7 @@ return {
     vim.g.mkdp_filetypes = { 'markdown' }
   end,
   ft = { 'markdown' },
+  config = function()
+    vim.keymap.set('n', '<leader>mp', '<cmd>MarkdownPreview<CR>', { desc = 'Markdown Preview' })
+  end,
 }
