496ea0f2d4bb00ee561ed7e6cca53070fa14011a
Bogdan Petrukovich <petrukovichbogdan@gmail.com>
2026-01-07 00:00:07 +0300

    comment mini.files

--- lua/custom/plugins/mini.lua
+++ lua/custom/plugins/mini.lua
@@ -8,6 +8,56 @@ return { -- Collection of various small independent plugins/modules
     --  - yinq - [Y]ank [I]nside [N]ext [']quote
     --  - ci'  - [C]hange [I]nside [']quote
     require('mini.ai').setup { n_lines = 500 }
+    -- require('mini.files').setup( -- No need to copy this inside `setup()`. Will be used automatically.
+    --   {
+    --     -- Customization of shown content
+    --     content = {
+    --       -- Predicate for which file system entries to show
+    --       filter = nil,
+    --       -- Highlight group to use for a file system entry
+    --       highlight = nil,
+    --       -- Prefix text and highlight to show to the left of file system entry
+    --       prefix = nil,
+    --       -- Order in which to show file system entries
+    --       sort = nil,
+    --     },
+    --
+    --     -- Module mappings created only inside explorer.
+    --     -- Use `''` (empty string) to not create one.
+    --     mappings = {
+    --       close = '<C-c>',
+    --       go_in = '<CR>', -- actions.select
+    --       -- go_in_plus = '<C-s>', -- vertical split
+    --       go_out = '-', -- actions.parent
+    --       go_out_plus = '_', -- go_out_plus как open_cwd
+    --       show_help = 'g?',
+    --       synchronize = '<C-s>', -- refresh/confirm
+    --       reveal_cwd = '`', -- actions.cd/open_cwd
+    --     },
+    --
+    --     -- General options
+    --     options = {
+    --       -- Whether to delete permanently or move into module-specific trash
+    --       permanent_delete = true,
+    --       -- Whether to use for editing directories
+    --       use_as_default_explorer = true,
+    --     },
+    --
+    --     -- Customization of explorer windows
+    --     windows = {
+    --       -- Maximum number of windows to show side by side
+    --       max_number = math.huge,
+    --       -- Whether to show preview of file/directory under cursor
+    --       preview = false,
+    --       -- Width of focused window
+    --       width_focus = 50,
+    --       -- Width of non-focused window
+    --       width_nofocus = 15,
+    --       -- Width of preview window
+    --       width_preview = 25,
+    --     },
+    --   }
+    -- )
 
     -- Add/delete/replace surroundings (brackets, quotes, etc.)
     --
