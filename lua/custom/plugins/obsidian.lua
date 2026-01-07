ae95056cb97dfa260d70f675bcc2d4802b8c32d5
Bogdan Petrukovich <petrukovichbogdan@gmail.com>
2026-01-07 14:31:37 +0300

    obsidian -> snacks picker

--- lua/custom/plugins/obsidian.lua
+++ lua/custom/plugins/obsidian.lua
@@ -159,20 +159,20 @@ return {
     picker = {
       -- Set your preferred picker. Can be one of 'telescope.nvim', 'fzf-lua', 'mini.pick' or 'snacks.pick'.
       -- TODO: change to snacks
-      name = 'telescope.nvim',
+      name = 'snacks.pick',
       -- Optional, configure key mappings for the picker. These are the defaults.
       -- Not all pickers support all mappings.
       note_mappings = {
         -- Create a new note from your query.
         new = '<C-x>',
         -- Insert a link to the selected note.
-        insert_link = '<C-l>',
+        insert_link = '<C-l>', -- TODO: wont work
       },
       tag_mappings = {
         -- Add tag(s) to current note.
         tag_note = '<C-x>',
         -- Insert a tag at the current location.
-        insert_tag = '<C-l>',
+        insert_tag = '<C-l>', -- TODO: wont work
       },
     },
 
