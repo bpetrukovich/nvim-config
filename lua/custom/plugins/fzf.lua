bd4554198fbb1b08f17ce10850e0ca480e3718de
Bogdan Petrukovich <petrukovichbogdan@gmail.com>
2026-01-07 14:31:57 +0300

    fzf keymaps

--- lua/custom/plugins/fzf.lua
+++ lua/custom/plugins/fzf.lua
@@ -5,8 +5,12 @@ return {
   -- or if using mini.icons/mini.nvim
   -- dependencies = { "nvim-mini/mini.icons" },
   config = function()
-    vim.keymap.set('n', '<leader>sg', function()
+    vim.keymap.set('n', '<leader>ff', function()
       require('fzf-lua').live_grep()
-    end, { desc = 'Grep (fzf)' })
+    end, { desc = 'Grep fzf' })
+
+    vim.keymap.set('n', '<leader>fr', function()
+      require('fzf-lua').resume()
+    end, { desc = 'fzf Resume' })
   end,
 }
