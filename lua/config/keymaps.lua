c667b8c36a86f10416871f8b234486b71fddd4a1
Bogdan Petrukovich <petrukovichbogdan@gmail.com>
2026-01-17 20:26:59 +0300

    fast copy current file path

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -19,6 +19,10 @@ vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
 
+vim.keymap.set('n', '<leader>yp', function()
+  vim.fn.setreg('+', vim.fn.expand '%')
+end, { desc = 'Yank relative path' })
+
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
 -- is not what someone will guess without a bit more experience.
