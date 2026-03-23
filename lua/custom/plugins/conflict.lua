cabed77990ea3b7d460d3cea83b7ac1554b3440a
bpetrukovich <petrukovichbogdan@gmail.com>
2026-03-23 18:31:23 +0300

    conflict

--- lua/custom/plugins/conflict.lua
+++ lua/custom/plugins/conflict.lua
@@ -4,4 +4,14 @@ return {
   opts = {
     default_mappings = false,
   },
+  config = function(opts)
+    require('git-conflict').setup(opts)
+
+    vim.keymap.set('n', '<leader>co', '<cmd>GitConflictChooseOurs<CR>')
+    vim.keymap.set('n', '<leader>ct', '<cmd>GitConflictChooseTheirs<CR>')
+    vim.keymap.set('n', '<leader>c2', '<cmd>GitConflictChooseBoth<CR>')
+    vim.keymap.set('n', '<leader>c0', '<cmd>GitConflictChooseNone<CR>')
+    vim.keymap.set('n', '[x', '<cmd>GitConflictNextConflict<CR>')
+    vim.keymap.set('n', ']x', '<cmd>GitConflictPrevConflict<CR>')
+  end,
 }
