0e40f7eac4d79ba8014c81e49765a19e583fd861
bpetrukovich <petrukovichbogdan@gmail.com>
2026-06-14 01:41:18 +0300

    remove unused from color.lua

--- lua/plugins/color.lua
+++ lua/plugins/color.lua
@@ -1,47 +1,12 @@
 return {
-  -- {
-  --   'rafamadriz/neon',
-  --   priority = 1000, -- Make sure to load this before all the other start plugins.
-  --   init = function()
-  --     vim.cmd.colorscheme 'neon'
-  --
-  --     vim.cmd.hi 'Comment gui=none'
-  --   end,
-  -- },
-  --
-  -- {
-  --   'bluz71/vim-moonfly-colors',
-  --   priority = 1000, -- Make sure to load this before all the other start plugins.
-  --   init = function()
-  --     vim.cmd.colorscheme 'moonfly'
-  --
-  --     -- You can configure highlights by doing something like:
-  --     vim.cmd.hi 'Comment gui=none'
-  --   end,
-  -- },
   {
     'rebelot/kanagawa.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
       vim.cmd.colorscheme 'kanagawa-dragon'
 
-      -- You can configure highlights by doing something like:
+      -- Remove italic font for comments
       vim.cmd.hi 'Comment gui=none'
     end,
   },
-  -- {
-  --   -- 'folke/tokyonight.nvim',
-  --   'rose-pine/neovim',
-  --   name = 'rose-pine-main',
-  --   priority = 1000, -- Make sure to load this before all the other start plugins.
-  --   init = function()
-  --     -- Load the colorscheme here.
-  --     -- Like many other themes, this one has different styles, and you could load
-  --     -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-  --     vim.cmd.colorscheme 'rose-pine'
-  --
-  --     -- You can configure highlights by doing something like:
-  --     vim.cmd.hi 'Comment gui=none'
-  --   end,
-  -- },
 }
