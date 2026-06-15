c1d9d877f7037524da64704c4751bb1bf20aaf00
bpetrukovich <petrukovichbogdan@gmail.com>
2026-06-15 16:15:40 +0300

    remove comment from csdebug

--- lua/plugins/csdebug.lua
+++ lua/plugins/csdebug.lua
@@ -5,8 +5,6 @@ return {
   config = function()
     require('dap-cs').setup {
       netcoredbg = {
-        -- the path to the executable netcoredbg which will be used for debugging.
-        -- by default, this is the "netcoredbg" executable on your PATH.
         path = vim.fn.expand '~/.local/share/nvim/mason/bin/netcoredbg',
       },
     }
